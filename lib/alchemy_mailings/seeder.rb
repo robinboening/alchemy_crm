fe67bf4b74ab2111df43cc27faa343b6c6e4032f
Thomas von Deyen <tvdeyen@gmail.com>
2011-04-29 18:32:07 +0200

    Seeder Status messages

--- lib/alchemy_mailings/seeder.rb
+++ lib/alchemy_mailings/seeder.rb
@@ -2,10 +2,11 @@ module AlchemyMailings
 	class Seeder
 		
 		def self.seed!
+			puts "Seeding database for Alchemy Mailing Module"
 			root = Page.root
 			lang = Language.get_default
 	    if root.nil? || lang.nil?
-				msg = "Aborting! Not all dependencies found. Did you ran the Alchemy seeder?"
+				puts "== Aborting! Not all dependencies found. Did you ran the Alchemy seeder?"
 			else
 	      mailing_root = Page.find_or_create_by_name(
 					:name => "Alchemy Mailings Rootpage",
@@ -17,12 +18,11 @@ module AlchemyMailings
 				if mailing_root.new_record?
 					mailing_root.save!
 	      	mailing_root.move_to_child_of root
-					msg = "Success! Mailing root page created"
+					puts "== Success! Mailing root page created"
 				else
-					msg = "Skipping! Already created mailing root page."
+					puts "== Skipping! Already created mailing root page."
 				end
 	    end
-			puts msg
 		end
 		
 	end
