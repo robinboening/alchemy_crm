d144d751fd2f1c3bfb17e544322bfdee78e5bc17
Thomas von Deyen <tvd@magiclabs.de>
2011-10-26 20:57:22 +0200

    Fixes locale domain name

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,3 @@
 require 'fast_gettext'
-FastGettext.add_text_domain 'alchemy_mailings', :path => File.join(File.dirname(__FILE__), '..', '..', 'locale'), :type => :po
-FastGettext.default_text_domain = 'alchemy_mailings'
+FastGettext.add_text_domain 'alchemy-mailings', :path => File.join(File.dirname(__FILE__), '..', '..', 'locale'), :type => :po
+FastGettext.default_text_domain = 'alchemy-mailings'
