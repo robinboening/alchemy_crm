47043c444d605023495de503fbca3ccff2713a7d
Thomas von Deyen <tvd@magiclabs.de>
2011-10-26 20:35:07 +0200

    Converted routes to rails 3 syntax

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,56 @@
-ActionController::Routing::Routes.draw do |map|
-  map.recipient_reads 'recipients/reads/:id', :controller => :recipients, :action => :reads
-  map.recipient_reacts 'recipients/reacts/:id', :controller => :recipients, :action => :reacts
-  map.resources :mailings, :only => :show
-  map.resources :contacts, :collection => {:signup => :post, :signout => :get, :verify => :get}, :except => [:index, :show, :new, :create, :edit, :update, :destroy]
-  map.verify_mailing '/contacts/verify/:sha1/:element_id', :controller => :contacts, :action => :verify
-  map.destroy_contact '/contacts/destroy/:sha1/:element_id', :controller => :contacts, :action => :destroy
-  map.signout_from_newsletter '/newsletter_subscriptions/destroy/:newsletter_id/:sha1/:element_id', :controller => :newsletter_subscriptions, :action => :destroy
-  map.teasable_elements '/admin/elements/teasables', :controller => 'admin/elements', :action => :teasables
-  map.resources :newsletter_subscriptions
-  map.namespace :admin do |admin|
-    admin.resources :contacts, :collection => {:import => :get}, :member => {:export => :get}
-    admin.resources :contact_groups, :collection => {:add_filter => :get}
-    admin.resources :tags, :newsletters
-    admin.resources :sent_mailings, :member => {:pdf => :get}
-    admin.resources :mailings, :member => {:copy => :get, :deliver => [:get, :post]}, :collection => {:signout => :get}, :has_many => :sent_mailings, :shallow => true
+Rails.application.routes.draw do
+
+  match '/recipients/reads/:id' => 'recipients#reads',
+    :as => 'recipient_reads'
+  match '/recipients/reacts/:id' => 'recipients#reacts',
+    :as => 'recipient_reacts'
+  match '/contacts/verify/:sha1/:element_id' => 'contacts#verify',
+    :as => 'verify_mailing'
+  match '/contacts/destroy/:sha1/:element_id' => 'contacts#destroy',
+    :as => 'destroy_contact'
+  match '/newsletter_subscriptions/destroy/:newsletter_id/:sha1/:element_id' => 'newsletter_subscriptions#destroy',
+    :as => 'signout_from_newsletter'
+  match '/admin/elements/teasables' => 'admin/elements#teasables',
+    :as => 'teasable_elements'
+
+  resources :mailings, :only => :show
+
+  resources :contacts, :except => [:index, :show, :new, :create, :edit, :update, :destroy] do
+    collection do 
+      post :signup
+      get :signout, :verify
+    end
   end
+
+  resources :newsletter_subscriptions
+
+  namespace :admin do
+
+    resources :contacts do
+      collection { get :import }
+      member { get :export }
+    end
+
+    resources :contact_groups do
+      collection { get :add_filter }
+    end
+
+    resources :tags, :newsletters
+
+    resources :sent_mailings do
+      member { get :pdf }
+    end
+
+    resources :mailings do
+      resources :sent_mailings
+      collection { get :signout }
+      member do
+        get :copy
+        get :deliver
+        post :deliver
+      end
+    end
+
+  end
+
 end
