7e4429815be4897c2d04bbd12697dc8e95b2166e
Thomas von Deyen <tvd@magiclabs.de>
2011-10-27 14:24:25 +0200

    Reverting back to element_extension and elements_controller_extension

--- lib/alchemy_mailings/models/element_extension.rb
+++ lib/alchemy_mailings/models/element_extension.rb
@@ -1,8 +1,31 @@
 module AlchemyMailings
 	module Models
 		module ElementExtension
-			
-		
+
+      def update_from_element(source, url)
+      	self.contents.each do |content|
+      		source_content = source.contents.find_by_name(content.name)
+      		if !source_content.blank? && source_content.essence_type == content.essence_type
+      			case content.essence_type
+      				when "EssenceText"
+      				content.essence.body = source_content.essence.body
+      				when "EssenceRichtext"
+      				content.essence.body = source_content.essence.body
+      				when "EssencePicture"
+      				content.essence.picture_id = source_content.essence.picture_id
+      				when "EssenceAttachment"
+      				content.essence.attachment_id = source_content.essence.attachment_id
+      			end
+      			content.essence.save
+      		end
+      	end
+      	teaser = self.contents.find_by_essence_type("EssenceElementTeaser")
+      	if !teaser.essence.blank?
+      		teaser.essence.url = url
+      		teaser.essence.save
+      	end
+      end
+
 		end
 	end
 end
