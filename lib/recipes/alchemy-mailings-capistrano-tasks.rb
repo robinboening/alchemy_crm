9c37193ae3cc617a7b6f19f24ca5dd14a019dfd6
Thomas von Deyen <tvd@magiclabs.de>
2011-10-27 15:23:26 +0200

    Fixes deployment recipe

--- lib/recipes/alchemy-mailings-capistrano-tasks.rb
+++ lib/recipes/alchemy-mailings-capistrano-tasks.rb
@@ -4,16 +4,7 @@ namespace 'alchemy-mailings' do
   
 	  desc "Copies all assets from Alchemy-Mailings plugin folder to public folder"
 	  task :copy do
-	    run "cd #{current_path} && RAILS_ENV=production rake alchemy-mailings:assets:copy:all"
-	  end
-  
-	end
-
-	namespace :db do
-  
-	  desc "Migrate Alchemy-Mailings database schema"
-	  task :migrate, :roles => :app, :except => { :no_release => true } do
-	    run "cd #{current_path} && RAILS_ENV=production rake db:migrate:alchemy-mailings"
+	    run "cd #{current_path} && RAILS_ENV=production #{rake} alchemy-mailings:assets:copy:all"
 	  end
   
 	end
