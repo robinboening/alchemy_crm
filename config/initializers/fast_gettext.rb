68a18b0e112a641411fbd58f6115dc6192ff1cce
Thomas von Deyen <tvd@magiclabs.de>
2011-10-28 11:02:52 +0200

    This plugin is not the default textdomain

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,3 +1,2 @@
 require 'fast_gettext'
 FastGettext.add_text_domain 'alchemy-mailings', :path => File.join(File.dirname(__FILE__), '..', '..', 'locale'), :type => :po
-FastGettext.default_text_domain = 'alchemy-mailings'
