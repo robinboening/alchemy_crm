60778618edc7d12caf1c38716f33f5e95e996d3a
Thomas von Deyen <tvd@magiclabs.de>
2011-10-28 19:20:58 +0200

    Removing engine because plugins are already engines

--- lib/alchemy_mailings.rb
+++ lib/alchemy_mailings.rb
@@ -5,7 +5,6 @@ if defined?(Rails) && Rails::VERSION::MAJOR == 3
   require 'alchemy_mailings/version'
   require 'alchemy_mailings/newsletter_layout'
   require "alchemy_mailings/seeder"
-  require 'alchemy_mailings/engine'
   require 'alchemy_mailings/models/element_extension'
   require 'alchemy_mailings/admin/elements_controller_extension'
 else
