7038425f0b7372a0c050802d685759a0e673c2ca
Thomas von Deyen <tvd@magiclabs.de>
2012-03-05 18:12:14 +0100

    Refactoring contact signup process

--- app/controllers/alchemy_crm/contacts_controller.rb
+++ app/controllers/alchemy_crm/contacts_controller.rb
@@ -3,36 +3,41 @@ module AlchemyCrm
 	class ContactsController < AlchemyCrm::BaseController
 
 		helper 'Alchemy::Pages'
+		layout :layout_for_page
 
 		def signup
 			@contact = Contact.new(params[:contact])
-			@element = Alchemy::Element.find(params[:element_id])
+			@element = Alchemy::Element.find_by_id(params[:element_id])
 			if @contact.save
-				followup_page = Alchemy::Page.find(@element.content_by_name('followup_page').ingredient)
-				MailingsMailer.deliver_verification_mail(
+				followup_page = Alchemy::Page.find(@element.ingredient('followup_page'))
+				ContactsMailer.signup_mail(
 					@contact,
-					current_server,
-					@element,
-					@contact.subscriptions.collect(&:newsletter_id)
-				)
+					@contact.subscriptions.collect(&:newsletter_id),
+					Alchemy::Page.find_by_page_layout_and_language_id('newsletter_mails', session[:language_id])
+				).deliver
 				redirect_to alchemy.show_page_path(:urlname => followup_page.urlname, :lang => multi_language? ? session[:language_code] : nil)
 			else
 				@page = @element.page
 				@root_page = @page.get_language_root
-				render :template => 'alchemy/pages/show', :layout => 'alchemy/pages'
+				render :template => 'alchemy/pages/show'
 			end
 		end
 
 		def verify
 			@element = Alchemy::Element.find(params[:element_id])
-			unless params[:sha1].blank?
-				@contact = Contact.find_by_email_sha1(params[:sha1])
-				@subscriptions = @contact.subscriptions.find_all_by_newsletter_id(params[:newsletter_ids])
+			if params[:token].blank?
+				flash[:notice] = 'Diese E-Mail Adresse ist uns nicht bekannt.'
+				@page = @element.page
+				@root_page = @page.get_language_root
+				render :template => 'alchemy/pages/show'
+			else
+				@contact = Contact.find_by_email_sha1(params[:token])
+				@subscriptions = @contact.subscriptions.where(:newsletter_id => params[:newsletter_ids])
 				@subscriptions.each do |subscription|
 					subscription.update_attributes(:verified => true)
 				end
 				@contact.update_attributes(:verified => true)
-				followup_page = Alchemy::Page.find(@element.content_by_name('verification_followup_page').ingredient)
+				followup_page = Alchemy::Page.find(@element.ingredient('followup_page'))
 				redirect_to alchemy.show_page_path(:urlname => followup_page.urlname, :lang => multi_language? ? session[:language_code] : nil)
 			end
 		end
@@ -41,11 +46,12 @@ module AlchemyCrm
 			@element = Alchemy::Element.find(params[:element_id])
 			@contact = Contact.find_by_email(params[:email])
 			if @contact.blank?
-				flash[:notice] = 'Diese Email Adresse ist uns nicht bekannt.'
+				flash[:notice] = 'Diese E-Mail Adresse ist uns nicht bekannt.'
 				@page = @element.page
-				render :template => 'alchemy/pages/show', :layout => 'alchemy/pages'
+				@root_page = @page.get_language_root
+				render :template => 'alchemy/pages/show'
 			else
-				followup_page = Alchemy::Page.find(@element.content_by_name('followup_page').ingredient)
+				followup_page = Alchemy::Page.find(@element.ingredient('followup_page'))
 				MailingsMailer.deliver_signout_mail(
 					@contact,
 					current_server,
@@ -57,9 +63,9 @@ module AlchemyCrm
 
 		def destroy
 			@element = Alchemy::Element.find(params[:element_id])
-			@contact = Contact.find_by_email_sha1(params[:sha1])
+			@contact = Contact.find_by_email_sha1(params[:token])
 			@contact.destroy
-			followup_page = Alchemy::Page.find(@element.content_by_name('signout_followup_page').ingredient)
+			followup_page = Alchemy::Page.find(@element.ingredient('followup_page'))
 			redirect_to alchemy.show_page_path(:urlname => followup_page.urlname, :lang => multi_language? ? session[:language_code] : nil)
 		rescue
 			log_error($!)
