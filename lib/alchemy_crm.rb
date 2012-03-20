de6cf16dcda45b517f6a62c39fbe49a96df2c8cf
Thomas von Deyen <tvd@magiclabs.de>
2012-03-20 12:28:55 +0100

    Removing prawn

--- lib/alchemy_crm.rb
+++ lib/alchemy_crm.rb
@@ -2,7 +2,6 @@ if defined?(Rails) && Rails::VERSION::MAJOR == 3
 	require 'acts-as-taggable-on'
 	require 'localized_country_select'
 	require 'rails3-jquery-autocomplete'
-	require "prawn"
 	require 'vcard'
 	require 'digest/sha1'
 	require 'delayed_job'
