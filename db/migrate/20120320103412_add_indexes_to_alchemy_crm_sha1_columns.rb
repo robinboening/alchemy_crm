16f7bbf50a2eccce24cf690c090e20a5d9597909
Thomas von Deyen <tvd@magiclabs.de>
2012-03-20 11:56:27 +0100

    Adding indexes for sha1 columns

---
+++ db/migrate/20120320103412_add_indexes_to_alchemy_crm_sha1_columns.rb
@@ -0,0 +1,7 @@
+class AddIndexesToAlchemyCrmSha1Columns < ActiveRecord::Migration
+  def change
+  	add_index :alchemy_crm_contacts, :email_sha1
+  	add_index :alchemy_crm_mailings, :sha1
+  	add_index :alchemy_crm_recipients, :sha1
+  end
+end
