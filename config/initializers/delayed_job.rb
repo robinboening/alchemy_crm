1cba20ac469f14dd5c9f66d6f39c19e000f1b5e9
Thomas von Deyen <tvd@magiclabs.de>
2012-03-22 12:33:34 +0100

    Updating delayed job config

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,5 +1,6 @@
+# config/initializers/delayed_job_config.rb
 Delayed::Worker.destroy_failed_jobs = false
-#Delayed::Worker.sleep_delay = 60
+Delayed::Worker.sleep_delay = 5
 Delayed::Worker.max_attempts = 1
-Delayed::Worker.max_run_time = 1.hours
+Delayed::Worker.max_run_time = 4.hours
 Delayed::Worker.delay_jobs = !Rails.env.test?
