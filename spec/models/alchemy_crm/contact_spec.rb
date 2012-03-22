3622a9d21a25b82ab586e0e2f6a0acd4089483de
Thomas von Deyen <tvd@magiclabs.de>
2012-03-22 12:30:43 +0100

    Modularize model specs

---
+++ spec/models/alchemy_crm/contact_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+module AlchemyCrm
+	describe Contact do
+
+		before(:all) do
+			@contact = Contact.create!({:email => 'jon@doe.com', :firstname => 'Jon', :lastname => 'Doe', :salutation => 'mr', :verified => true}, :as => :admin)
+		end
+
+		describe '.create' do
+
+			it "should set email_salt and email_sha1 for new records" do
+				@contact.email_sha1.should_not be_nil
+				@contact.email_salt.should_not be_nil
+			end
+
+		end
+
+		describe '#save' do
+
+			context "a contact with new email address" do
+
+				before(:each) do
+					@sha1 = @contact.email_sha1
+					@salt = @contact.email_salt
+					@contact.update_attributes!(:email => 'jane@doe.com')
+				end
+
+				it "should update email_sha1" do
+					@contact.email_sha1.should_not == @sha1
+				end
+
+				it "should not update salt" do
+					@contact.email_salt.should == @salt
+				end
+
+			end
+
+			context "a contact with same email address" do
+
+				before(:each) do
+					@sha1 = @contact.email_sha1
+					@salt = @contact.email_salt
+					@contact.update_attributes!(:firstname => 'jane')
+				end
+
+				it "should not update email_sha1" do
+					@contact.email_sha1.should == @sha1
+				end
+
+				it "should not update salt" do
+					@contact.email_salt.should == @salt
+				end
+
+			end
+
+		end
+
+		after(:all) do
+			@contact.destroy
+		end
+
+	end
+end
---
+++ spec/models/alchemy_crm/delivery_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+module AlchemyCrm
+	describe Delivery do
+
+		describe '#deliver!' do
+
+			before(:each) do
+				@emails = ["jim@family.com", "jon@doe.com", "jane@family.com", "john@gmail.com", "jaja@binks.com", "lulu@fame.org"]
+				@mailing = Mailing.create!(
+					:name => 'Mailing',
+					:additional_email_addresses => @emails.join(', '),
+					:newsletter => Newsletter.create!(:name => 'Newsletter', :layout => 'standard')
+				)
+				@delivery = Delivery.create!(:mailing => @mailing)
+				@recipients = @delivery.recipients
+			end
+
+			it "should deliver mailings to recipients in chunks." do
+				AlchemyCrm::Delivery.stub!(:settings).and_return(2)
+				@delivery.should_receive(:send_mail_chunk).once.with(@recipients[0..1])
+				@delivery.should_receive(:send_mail_chunk).once.with(@recipients[2..3])
+				@delivery.should_receive(:send_mail_chunk).once.with(@recipients[4..5])
+				@delivery.deliver!
+			end
+
+		end
+
+	end
+end
