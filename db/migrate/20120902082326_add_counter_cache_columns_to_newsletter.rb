9f9493f54c6cda8ac9f427be8d9a270f4293da58
Robin Böning <rb@magiclabs.de>
2012-09-02 18:39:05 +0200

    Converts tabs into spaces.

--- db/migrate/20120902082326_add_counter_cache_columns_to_newsletter.rb
+++ db/migrate/20120902082326_add_counter_cache_columns_to_newsletter.rb
@@ -1,7 +1,7 @@
 class AddCounterCacheColumnsToNewsletter < ActiveRecord::Migration
   def change
-  	add_column :alchemy_crm_newsletters, :subscriptions_count, :integer, :default => 0, :nil => false
-  	add_column :alchemy_crm_newsletters, :user_subscriptions_count, :integer, :default => 0, :nil => false
-  	add_column :alchemy_crm_newsletters, :contact_group_subscriptions_count, :integer, :default => 0, :nil => false
+    add_column :alchemy_crm_newsletters, :subscriptions_count, :integer, :default => 0, :nil => false
+    add_column :alchemy_crm_newsletters, :user_subscriptions_count, :integer, :default => 0, :nil => false
+    add_column :alchemy_crm_newsletters, :contact_group_subscriptions_count, :integer, :default => 0, :nil => false
   end
 end
