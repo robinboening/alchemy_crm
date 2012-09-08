f742c0c5916068158890054781684b9f6d70950c
Robin Böning <rb@magiclabs.de>
2012-09-08 17:12:48 +0200

    Removes mailing_id from Recipient.

--- db/migrate/20120906181848_add_mailing_id_to_recipients.rb
+++
@@ -1,5 +0,0 @@
-class AddMailingIdToRecipients < ActiveRecord::Migration
-  def change
-  	add_column :alchemy_crm_recipients, :mailing_id, :integer
-  end
-end
--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120906181848) do
+ActiveRecord::Schema.define(:version => 20120904001234) do
 
   create_table "alchemy_attachments", :force => true do |t|
     t.string   "name"
@@ -168,7 +168,6 @@ ActiveRecord::Schema.define(:version => 20120906181848) do
     t.datetime "updated_at"
     t.string   "sha1"
     t.string   "salt"
-    t.integer  "mailing_id"
   end
 
   add_index "alchemy_crm_recipients", ["contact_id"], :name => "index_alchemy_crm_recipients_on_contact_id"
